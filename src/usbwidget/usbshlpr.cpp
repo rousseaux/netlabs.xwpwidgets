982f9554d452fedcfc2c1f5a4be9572de36c7f61
Ben Rietbroek <rousseau.os2dev@gmx.com>
2016-09-22 23:52:17 +0200

    usbwidget: Coverted the Daemon to C++
    This is done 'quick-n-dirty' for now.
    Going C++ will make it easier to also use the daemon for some basic
    component testing. And when changes are made, there is no need to
    restart the WPS, as is the case with the widget.

--- src/usbwidget/usbshlpr.cpp
+++ src/usbwidget/usbshlpr.cpp
@@ -23,18 +23,41 @@
 \*****************************************************************************/
 
 
-/**
- * This module implements a DAEMON to supply named shared memory to the
- * USB Widget. It allocates a memory pool and initializes the start with a
- * custom structure understood by the Widget. The rest is initialized by
- * DosSetSubMem to enable the use of DosSubAllocMem by the Widget.
- *
- * The Widget attaches to this named shared memory and has it's C malloc and
- * free functions overridden to use DosSubAllocMem. Also, the C++ new and
- * delete operators are overloaded to use the overridden malloc and free
- * functions. This enables the Widget to retain it's state between WPS
- * restarts, especially the drive <-> usb-device relations.
- */
+/******************************************************************************
+* This module implements a DAEMON that keeps shared memory from being released
+* -----------------------------------------------------------------------------
+* It is not directly possible to identify which usb-device holds the mass
+* storage device where one or more drives are mounted on. These relations are
+* held in a list which is stored in the shared memory. If the WPS is restarted,
+* the Widget can re-use the list to know which drives are mounted on which
+* usb-devices.
+*
+* The shared memory is initialized at the start with a custom structure which
+* also understood by the Widget. The rest is initialized by DosSetSubMem to
+* enable the use of DosSubAllocMem by the Widget. The Widget attaches to this
+*  named shared memory and has it's C malloc and free functions overridden to
+* use DosSubAllocMem. Also, the C++ new and delete operators are overloaded
+* to use the overridden malloc and free functions. This enables the Widget to
+*  retain it's state between WPS restarts.
+*
+*/
+
+/*
+// Phantom Drives
+// --------------
+// When an usb-device with a drive mounted on it is removed without properly
+// ejecting it, the drive does not disappear. Ejecting the drive will make it
+// disappear, but only until the removable media are rescanned, after which it
+// will reappear. Such a drive is called a *phantom* drive. Because the backing
+// medium is missing, such a drive is of course not accessible. However,
+// reinserting the _exact_ same usb-device will make the drive functional
+// again. But, insering a _different_ usb-device will most probably cause a
+// crash in LVM. The widget is capable of recovering phantom drives when the
+// correct usb-device is interted again.
+*/
+
+
+//! ----------------------------------------------------------------- [includes]
 
 #include    "AllocMem.hpp"
 
@@ -49,43 +72,141 @@
 */
 #include    "usbshlpr.lvl"
 
+//! ----------------------------------------------------------------- [globals]
+
 /* Pointer to the named shared memory */
-void*   shmem = NULL;
-int     deamon = FALSE;
+//~ void*   shmem = NULL;
+//~ int     deamon = FALSE;
 
+//! ------------------------------------------------------------------ [Daemon]
 
-/*
-// Start the Daemon; this is the default action.
-*/
-int     startd() {
-    int             rc          = 0;
-    APIRET          ulrc        = -1;
-    PMASTERMEMPOOL  pmmp        = NULL;
-    HEV             wait4daemon = NULL;
-    HEV             wait4stop   = NULL;
+Daemon::Daemon() {
+    __ctorb();
+    this->clear();
+    __ctore();
+}
+
+//~ Daemon::Daemon(int argc, char* argv[]) {
+    //~ __ctorb();
+    //~ this->clear();
+    //~ this->argc = argc;
+    //~ this->argv = argv;
+    //~ __ctore();
+//~ }
+
+Daemon::~Daemon() {
+    __dtorb();
+    this->clear();
+    __dtore();
+}
+
+void    Daemon::clear() {
+    __mthd();
+    this->argc          = 0;
+    this->argv          = NULL;
+    this->wait4daemon   = NULL;
+    this->wait4stop     = NULL;
+}
+
+int     Daemon::doAction() {
+    __mthd();
+    printf("argc      : %d\n", this->argc);
+    printf("argv[0]   : %s\n", this->argv[0]);
+    printf("wait4stop : %08X\n", this->wait4stop);
+
+    do {
+        int     rc      = 0;
+        BOOL    stop    = FALSE;
+        BOOL    start   = FALSE;
+        int     action  = 0;
+
+        /* Args present see if stop is the first (no check for other args) */
+        if (this->argc > 1) {
+            stop    = !strcmpi(argv[1], "/stop");
+            action  = stop;
+        }
+
+        /* Only 1 stop is recognized, the rest is start */
+        switch (action)
+            case    1: {
+                rc = this->stop();
+                break;
+            default:
+                rc = this->start();
+                break;
+        }
+
+    } while (0);
+
+    return 0;
+}
+
+int     Daemon::start() {
+    __mthd();
+    return 0;
+}
+
+int     Daemon::stop() {
+    __mthd();
+    return 0;
+}
+
+//! --------------------------------------------------------------- [CliDaemon]
+
+CliDaemon::CliDaemon(int argc, char* argv[]) {
+    __ctorb();
+    this->clear();
+    this->argc = argc;
+    this->argv = argv;
+    __ctore();
+}
+
+CliDaemon::~CliDaemon() {
+    __dtorb();
+    this->clear();
+    __dtore();
+}
+
+void    CliDaemon::clear() {
+    __mthd();
+    this->pmmp          = NULL;
+    //~ this->Daemon::clear();
+}
+
+int     CliDaemon::start() {
+    int     rc      = 0;
+    APIRET  ulrc    = -1;
+    __mthd();
 
     /* Try to allocate the named shared memory */
-    ulrc = DosAllocSharedMem(
-                &shmem,
+    ulrc =  DosAllocSharedMem(
+                (PPVOID) &this->pmmp,
                 SHMEM_NAME,
                 SHMEM_SIZE+sizeof(MASTERMEMPOOL)+sizeof(SUBMEMPOOL),
                 PAG_COMMIT | PAG_READ | PAG_WRITE
             );
 
-
     if (ulrc) {
         switch (ulrc) {
-            case ERROR_ALREADY_EXISTS:
-                printf("The Shared Memory (%s) already exists.\n", SHMEM_NAME);
+            case ERROR_ALREADY_EXISTS: {
+                PVOID   p = NULL;
+                ulrc =  DosGetNamedSharedMem(
+                            &p,
+                            SHMEM_NAME,
+                            PAG_READ | PAG_WRITE
+                        );
+                printf("The Shared Memory (%s) already exists at: 0x%08x.\n", SHMEM_NAME, p);
+                ulrc = -1;
                 return 0;
-            break;
-            default:
+                break;
+            }
+            default: {
                 printf("Error %d occured, aborting.\n", ulrc);
                 return 0;
-            break;
+                break;
+            }
         }
     }
-
 
     printf(
         "DosAllocSharedMem: size: %d, ulrc: %d\n",
@@ -94,31 +215,31 @@ int     startd() {
     );
 
     /* Open the semaphore that the Widget created */
-    ulrc = DosOpenEventSem(EVSEM_WAIT4DAEMON, &wait4daemon);
+    ulrc = DosOpenEventSem(EVSEM_WAIT4DAEMON, &this->wait4daemon);
     printf("DosOpenEventSem: ulrc: %d\n", ulrc);
 
     /* First try to allocate the Master Pool */
     if (!ulrc) {
-        pmmp = (PMASTERMEMPOOL) shmem;          // Cast pointer to access master structure
-        pmmp->signature     = SHMEM_SIGNATURE;  // Set signature
-        pmmp->poolsize      = SHMEM_SIZE+sizeof(MASTERMEMPOOL)+sizeof(SUBMEMPOOL);
-        pmmp->pooltype      = PT_USE_NAMED;     // Set the pooltype
-        pmmp->submempool    = &pmmp[1];         // Sub Memory Pool starts here
-        pmmp->pwidgetstate  = NULL;             // Pointer to the Widget State
+        //~ this->pmmp = (PMASTERMEMPOOL) shmem;          // Cast pointer to access master structure
+        this->pmmp->signature     = SHMEM_SIGNATURE;  // Set signature
+        this->pmmp->poolsize      = SHMEM_SIZE+sizeof(MASTERMEMPOOL)+sizeof(SUBMEMPOOL);
+        this->pmmp->pooltype      = PT_USE_NAMED;     // Set the pooltype
+        this->pmmp->submempool    = &this->pmmp[1];         // Sub Memory Pool starts here
+        this->pmmp->pwidgetstate  = NULL;             // Pointer to the Widget State
 
 
         printf(
             "Master Pool: address:%08lX, signature:%08lX, poolsize:%d, pooltype:%d, submempool:%08lX, widgetstate: %08lX\n",
-            pmmp,
-            pmmp->signature,
-            pmmp->poolsize,
-            pmmp->pooltype,
-            pmmp->submempool,
-            pmmp->pwidgetstate);
+            this->pmmp,
+            this->pmmp->signature,
+            this->pmmp->poolsize,
+            this->pmmp->pooltype,
+            this->pmmp->submempool,
+            this->pmmp->pwidgetstate);
 
         /* Now try to create the sub-allocation system */
         ulrc = DosSubSetMem(
-                    pmmp->submempool,
+                    this->pmmp->submempool,
                     DOSSUB_INIT,
                     SHMEM_SIZE+sizeof(SUBMEMPOOL)
                 );
@@ -129,111 +250,89 @@ int     startd() {
             //~ _getch();                               // Wait for a key press
             //!: SubSetMem Beep
             //~ DosBeep(3000,200);
-            ulrc = DosPostEventSem(wait4daemon);
+            ulrc = DosPostEventSem(this->wait4daemon);
             printf("DosPostEventSem: ulrc: %d\n", ulrc);
-            ulrc = DosCloseEventSem(wait4daemon);
+            ulrc = DosCloseEventSem(this->wait4daemon);
             printf("DosCloseEventSem: ulrc: %d\n", ulrc);
 
-            ulrc = DosCreateEventSem(EVSEM_WAIT4STOP, &wait4stop, DC_SEM_SHARED, FALSE);
+            ulrc = DosCreateEventSem(EVSEM_WAIT4STOP, &this->wait4stop, DC_SEM_SHARED, FALSE);
             printf("DosCreateEventSem: ulrc: %d\n", ulrc);
 
             //!: BOOKMARK: Block on semaphore
             printf("DosWaitEventSem: blocking...\n");
             fflush(stdout);
-            ulrc = DosWaitEventSem(wait4stop, SEM_INDEFINITE_WAIT);
+            ulrc = DosWaitEventSem(this->wait4stop, SEM_INDEFINITE_WAIT);
             printf("DosWaitEventSem: ulrc: %d\n", ulrc);
 
-            ulrc = DosCloseEventSem(wait4stop);
+            ulrc = DosCloseEventSem(this->wait4stop);
             printf("DosCloseEventSem: ulrc: %d\n", ulrc);
-
-
-
-
 
             //~ DosSleep(120000);
             //!: SubSetMem Beep 2
             //~ DosBeep(1000,200);
-            ulrc = DosSubUnsetMem(pmmp->submempool); // Cleanup sub-allocation system
+            ulrc = DosSubUnsetMem(this->pmmp->submempool); // Cleanup sub-allocation system
             printf("DosSubUnsetMem: ulrc: %d\n", ulrc);
 
-            ulrc = DosFreeMem(shmem);               // Free the Master Pool
+            ulrc = DosFreeMem(this->pmmp);               // Free the Master Pool
             printf("DosFreeMem: ulrc: %d\n", ulrc);
 
-            shmem = NULL;
+            this->pmmp = NULL;
             rc = ulrc;
-
 
         }
         /* Setting up the sub-allocation system failed */
         else {
-            DosFreeMem(shmem);          // Free the Master Pool
+            DosFreeMem(this->pmmp);          // Free the Master Pool
             printf("DosSubSetMem: ulrc: %d\n", ulrc);
 
-            shmem = NULL;
+            this->pmmp = NULL;
             rc = ulrc;
-
         }
     }
     /* Allocating the Master Pool failed */
     else {
-        shmem = NULL;
+        this->pmmp = NULL;
         rc = ulrc;
     }
 
     return rc;
 }
 
-/*
-// Stop the Daemon; when /stop argument found.
-*/
-int     stopd() {
-    int             rc          = 0;
-    APIRET          ulrc        = -1;
-    PMASTERMEMPOOL  pmmp        = NULL;
-    HEV             wait4daemon = NULL;
-    HEV             wait4stop   = NULL;
+int     CliDaemon::stop() {
+    APIRET  ulrc    = -1;
+    __mthd();
 
-    ulrc = DosOpenEventSem(EVSEM_WAIT4STOP, &wait4stop);
+    ulrc = DosOpenEventSem(EVSEM_WAIT4STOP, &this->wait4stop);
     printf("DosOpenEventSem  : ulrc: %d\n", ulrc);
-    ulrc = DosPostEventSem(wait4stop);
+    ulrc = DosPostEventSem(this->wait4stop);
     printf("DosPostEventSem  : ulrc: %d\n", ulrc);
-    ulrc = DosCloseEventSem(wait4stop);
+    ulrc = DosCloseEventSem(this->wait4stop);
     printf("DosCloseEventSem : ulrc: %d\n", ulrc);
-
-    //~ rc = ulrc;
 
     return 0;
 }
 
-
+//! -------------------------------------------------------------------- [main]
 
 /*
-// Allocate the named shared memory and wait for a key-press.
+// Main entrypoint
 */
 int     main(int argc, char* argv[]) {
-    int             rc          = 0;
-    APIRET          ulrc        = -1;
-    PMASTERMEMPOOL  pmmp        = NULL;
-    HEV             wait4daemon = NULL;
-    BOOL            stop        = FALSE;
-    BOOL            start       = FALSE;
-    int             action      = 0;
+    int     rc  = 0;
 
-    /* Args present see if stop is the first (no check for other args) */
-    if (argc > 1) {
-        stop    = !strcmpi(argv[1], "/stop");
-        action  = stop;
-    }
-
-    /* Only 1 stop is recognized, the rest is start */
-    switch (action)
-        case    1: {
-            rc = stopd();
-            break;
-        default:
-            rc = startd();
-            break;
-    }
+    /* Create a Daemon Instance and give it control */
+    do {
+        Daemon*         daemon      = NULL;
+        daemon = new CliDaemon(argc, argv);
+        //~ daemon = new CliDaemon();
+        if (daemon) {
+            printf("-------------------------------------------------------------------------------\n");
+            rc = daemon->doAction();
+            printf("-------------------------------------------------------------------------------\n");
+            delete daemon;
+        }
+    } while (0);
+    return rc;
 
     /* Return to system */
     printf("usbshlpr.dmn: rc: %d\n", rc);
--- src/usbwidget/usbshlpr.hpp
+++ src/usbwidget/usbshlpr.hpp
@@ -23,11 +23,38 @@
 \*****************************************************************************/
 
 
-/**
- * This is the public header file for it's corresponding source.
- * It includes other public header files so it can use the stuff declared
- * there.
- */
+/******************************************************************************
+* This module implements a DAEMON that keeps shared memory from being released
+* -----------------------------------------------------------------------------
+* It is not directly possible to identify which usb-device holds the mass
+* storage device where one or more drives are mounted on. These relations are
+* held in a list which is stored in the shared memory. If the WPS is restarted,
+* the Widget can re-use the list to know which drives are mounted on which
+* usb-devices.
+*
+* The shared memory is initialized at the start with a custom structure which
+* also understood by the Widget. The rest is initialized by DosSetSubMem to
+* enable the use of DosSubAllocMem by the Widget. The Widget attaches to this
+*  named shared memory and has it's C malloc and free functions overridden to
+* use DosSubAllocMem. Also, the C++ new and delete operators are overloaded
+* to use the overridden malloc and free functions. This enables the Widget to
+*  retain it's state between WPS restarts.
+*
+*/
+
+/*
+// Phantom Drives
+// --------------
+// When an usb-device with a drive mounted on it is removed without properly
+// ejecting it, the drive does not disappear. Ejecting the drive will make it
+// disappear, but only until the removable media are rescanned, after which it
+// will reappear. Such a drive is called a *phantom* drive. Because the backing
+// medium is missing, such a drive is of course not accessible. However,
+// reinserting the _exact_ same usb-device will make the drive functional
+// again. But, insering a _different_ usb-device will most probably cause a
+// crash in LVM. The widget is capable of recovering phantom drives when the
+// correct usb-device is interted again.
+*/
 
 
 #ifndef     __USBSHLPR_HPP__
@@ -37,12 +64,62 @@
 #endif
 
 #include    <conio.h>
-#include    "ModuleGlobals.hpp"
-
+#include    "Master.hpp"
 
 /* Prototypes */
-int     startd();
-int     stopd();
+//~ int     startd();
+//~ int     stopd();
+
+/* Class Declarations */
+class   Daemon;
+class   CliDaemon;
+
+// ----------------------------------------------------------------------------
+// Daemon :: Kinda provides the interface with no real implementation
+// ----------------------------------------------------------------------------
+class   Daemon {
+
+    public:
+
+    /* Constructor(s) and Destructor */
+    Daemon();
+    virtual ~Daemon();
+
+    /* Public Methods */
+    virtual int     doAction();     // Determines the action to take
+    virtual int     start();        // Start the Daemon
+    virtual int     stop();         // Stop the Daemon
+
+    /* Public Attributes */
+    int             argc;           // Argument count passed from main()
+    char**          argv;           // Array of argument pointers passed from main()
+    HEV             wait4daemon;    // UsbWidget blocks on this until Daemon ready
+    HEV             wait4stop;      // Daemon blocks on this (/stop posts it)
+
+    protected:
+    virtual void    clear();        // Clear instance attributes
+
+    private:
+};
+
+// ----------------------------------------------------------------------------
+// CliDaemon :: Provides the implementation for the Daemon
+// ----------------------------------------------------------------------------
+// This class is used when the daemon is started from the command-line,
+// wether it is detached or not.
+// ----------------------------------------------------------------------------
+class   CliDaemon : public Daemon {
+    public:
+    CliDaemon(int argc, char* argv[]);
+    virtual ~CliDaemon();
+    virtual int     start();        // Override default implementation
+    virtual int     stop();         // Override default implemenation
+    PMASTERMEMPOOL  pmmp;           // Pointer to shared memory
+    protected:
+    virtual void    clear();        // Clear instance attributes
+    private:
+    char    buf[512];               // Work buffer
+};
 
 
 #ifdef      __cplusplus
